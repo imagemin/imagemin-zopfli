e1b01dcddbc222f5deb2931193c9f68a10455476
Kevin Martensson <kevinmartensson@gmail.com>
2014-10-21 16:49:56 +0200

    Provide a reusable constructor

--- index.js
+++ index.js
@@ -6,7 +6,57 @@ var through = require('through2');
 var zopfli = require('zopflipng-bin').path;
 
 /**
- * zopfli image-min plugin
+ * zopfli imagemin plugin
+ *
+ * @param {Object} file
+ * @param {String} enc
+ * @param {Object} opts
+ * @param {Function} cb
+ * @api private
+ */
+
+function plugin(file, enc, opts, cb) {
+	if (file.isNull()) {
+		cb(null, file);
+		return;
+	}
+
+	if (file.isStream()) {
+		cb(new Error('Streaming is not supported'));
+		return;
+	}
+
+	if (!isPng(file.contents)) {
+		cb(null, file);
+		return;
+	}
+
+	var exec = new ExecBuffer();
+	var args = ['-y'];
+
+	if (opts.more) {
+		args.push('-m');
+	}
+
+	if (opts['8bit']) {
+		args.push('--lossy_8bit');
+	}
+
+	exec
+		.use(zopfli, args.concat([exec.src(), exec.dest()]))
+		.run(file.contents, function (err, buf) {
+			if (err) {
+				cb(err);
+				return;
+			}
+
+			file.contents = buf;
+			cb(null, file);
+		});
+}
+
+/**
+ * Module exports
  *
  * @param {Object} opts
  * @api public
@@ -15,43 +65,22 @@ var zopfli = require('zopflipng-bin').path;
 module.exports = function (opts) {
 	opts = opts || {};
 
-	return through.obj(function (file, imagemin, cb) {
-		if (file.isNull()) {
-			cb(null, file);
-			return;
-		}
+	return through.obj(function (file, enc, cb) {
+		plugin(file, enc, opts, cb);
+	});
+};
 
-		if (file.isStream()) {
-			cb(new Error('Streaming is not supported'));
-			return;
-		}
+/**
+ * Module exports constructor
+ *
+ * @param {Object} opts
+ * @api public
+ */
 
-		if (!isPng(file.contents)) {
-			cb(null, file);
-			return;
-		}
+module.exports.ctor = function (opts) {
+	opts = opts || {};
 
-		var exec = new ExecBuffer();
-		var args = ['-y'];
-
-		if (opts.more) {
-			args.push('-m');
-		}
-
-		if (opts['8bit']) {
-			args.push('--lossy_8bit');
-		}
-
-		exec
-			.use(zopfli, args.concat([exec.src(), exec.dest()]))
-			.run(file.contents, function (err, buf) {
-				if (err) {
-					cb(err);
-					return;
-				}
-
-				file.contents = buf;
-				cb(null, file);
-			});
+	return through.ctor({ objectMode: true }, function (file, enc, cb) {
+		plugin(file, enc, opts, cb);
 	});
 };
